4a3ab398c62d4bd41b09cf72fa14c3f0283ff540
Yogndr <106022206+Yogndr@users.noreply.github.com>
2024-12-26 13:07:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function firstWord(s) {
+	let str=s.trim();
+	if(str.length==0)
+		return str;
+	let res="";
+	for(let i=0;i<str.length;i++){
+		if(str[i]==" ")
+			break;
+		else
+			res+=str[i];
+	}
+	return res;
   // your code here
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+// const s = prompt("Enter String:");
 alert(firstWord(s));
